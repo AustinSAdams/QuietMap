5b446e71a596d4dae88fa42c503e49ea46f8a678
Austin Adams <asadams110102@gmail.com>
2025-02-06 20:40:15 -0600

    Removed unnecessary curly braces.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
+use colored::*;
 use rand::Rng;
+use std::env;
+use std::fs::{create_dir_all, File};
 use std::io::{self, Write};
 use std::path::PathBuf;
-use std::fs::{File, create_dir_all};
-use std::env;
-use std::process::{Command};
+use std::process::Command;
 use tokio::time::{sleep, Duration};
-use colored::*;
 
 fn generate_number(min: i32, max: i32) -> i32 {
     let mut rng = rand::thread_rng();
@@ -14,7 +14,7 @@ fn generate_number(min: i32, max: i32) -> i32 {
 
 async fn wait(seconds: u64) {
     let mut remaining_seconds = seconds;
-    
+
     while remaining_seconds > 0 {
         let time_remaining = format_seconds(remaining_seconds);
         print!("{}", format!("\rWaiting for {}", time_remaining).yellow());
@@ -62,7 +62,11 @@ fn map_target(ip: &str, port: &str, scan_type: Option<&str>, output_prefix: &str
         Some(_) => "",
     };
 
-    let file_name = format!("{PREFIX}_{TYPE}", PREFIX=output_prefix, TYPE=scan_type_str);
+    let file_name = format!(
+        "{PREFIX}_{TYPE}",
+        PREFIX = output_prefix,
+        TYPE = scan_type_str
+    );
     match create_file(&file_name) {
         Ok(mut file) => {
             let mut command = Command::new("sudo");
@@ -79,19 +83,29 @@ fn map_target(ip: &str, port: &str, scan_type: Option<&str>, output_prefix: &str
             let output = command.output().expect(&"Failed to execute command".red());
 
             if output.status.success() {
-                file.write_all(&output.stdout).expect(&"Failed to write output to file".red());
-                println!("\nnmap {} {} {}",
-                    ip, 
-                    scan_type.unwrap_or(""), 
+                file.write_all(&output.stdout)
+                    .expect(&"Failed to write output to file".red());
+                println!(
+                    "\nnmap {} {} {}",
+                    ip,
+                    scan_type.unwrap_or(""),
                     if port.trim().is_empty() {
-                        "".to_string() 
-                    } else { 
-                        format!("-p {}", port) 
-                    });
+                        "".to_string()
+                    } else {
+                        format!("-p {}", port)
+                    }
+                );
 
                 println!("{}", format!("Output Stored In: {}.txt", file_name).green());
             } else {
-                eprintln!("{}", format!("Nmap command failed with error: {}", String::from_utf8_lossy(&output.stderr)).red());
+                eprintln!(
+                    "{}",
+                    format!(
+                        "Nmap command failed with error: {}",
+                        String::from_utf8_lossy(&output.stderr)
+                    )
+                    .red()
+                );
             }
         }
         Err(e) => eprintln!("{}", format!("Error creating file: {}", e).red()),
@@ -108,24 +122,35 @@ fn main() {
     let mut target_ip = String::new();
     print!("\nTarget IP: ");
     io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut target_ip).expect(&"Failed to read input.".red());
+    io::stdin()
+        .read_line(&mut target_ip)
+        .expect(&"Failed to read input.".red());
 
     let mut target_port = String::new();
     print!("Target Port (Enter for ALL): ");
     io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut target_port).expect(&"Failed to read input.".red());
+    io::stdin()
+        .read_line(&mut target_port)
+        .expect(&"Failed to read input.".red());
 
     let mut file_prefix = String::new();
     print!("Output File Prefix: ");
     io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut file_prefix).expect(&"Failed to read input.".red());
+    io::stdin()
+        .read_line(&mut file_prefix)
+        .expect(&"Failed to read input.".red());
 
     let mut mode_choice = String::new();
     print!("Choose Mode (Stealth / Fast): ");
     io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut mode_choice).expect(&"Failed to read input.".red());
+    io::stdin()
+        .read_line(&mut mode_choice)
+        .expect(&"Failed to read input.".red());
 
-    println!("{}", format!("Output Directory: {}", map_data_dir.display()).green());
+    println!(
+        "{}",
+        format!("Output Directory: {}", map_data_dir.display()).green()
+    );
 
     let mode_choice = mode_choice.trim().to_lowercase();
     let (min_wait_time, max_wait_time) = if mode_choice == "fast" {
